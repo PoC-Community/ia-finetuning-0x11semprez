44432f9589d5ee8fd0e0b45728d5cd113bb8df34
0x11semprez <traoresemprez@icloud.com>
2025-11-05 19:14:24 +0100

    think is that

--- mymodel.py
+++ mymodel.py
@@ -1,13 +1,10 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-# Load tokenizer and model
+
 model_name = 'gpt2'
-
-tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
-model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2", dtype=torch.float16, device_map="auto", attn_implementation="sdpa")
-
-# Set pad token (because the end of the sentence is not detected by the model)
-tokenizer.pad_token =
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer.pad_token = None
 
 print(f"✅ Model '{model_name}' loaded successfully!")
 print(f"Model has {model.num_parameters():,} parameters")
